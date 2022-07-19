53f9b719faea8157315cb144f776ad1fc6c36f14
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-07-19 11:48:47 +0530

    Inheritance and polymorphism

--- JavaWebDevelopment/src/main/java/Oops/Singleinheritance.java
+++ JavaWebDevelopment/src/main/java/Oops/Singleinheritance.java
@@ -6,6 +6,46 @@ public class Singleinheritance
 	{
 		System.out.println("father is familyman ");
 	}
-
-
 }
+
+
+//Inheritance:
+//--------------
+//Inheritance the properties of parent into child class
+//
+//Inheritance is achieved by using the word is "extends";
+//
+//IS_A relationship......Reusability
+//
+//why are using inheritance:
+//---------------------------
+//
+//Polymorphism ovrriding
+//
+//code reusability
+//
+//
+//Inheritance four types used in java:
+//------------------------------------
+//single
+//multiple--java doest support....that time we are using interface(implements)..
+//multilevel
+//hierachical
+//
+//
+//POLYMORPHISM:
+//
+//full form-Many forms......example:water--soild,liquid,gas
+//				  shape-circle,rectangle,square
+//
+//Single action but different behaviours......to ability to take more the one forms.....
+//
+//
+//two types of polymorphism used in jave;
+//
+//1.Compile time polymorphism-----method overloading.....
+//Same class,method name same,Different agruments or parametrized....
+//
+//
+//Run time polymorphism--------method overriding....
+//Different class,method same,same as arguments or paramerters
